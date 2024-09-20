b938f49deb06195feb531e5b7cd1a2f71a96ff60
aleks <aleksejserakov45@gmail.com>
2024-09-20 17:38:03 +0700

    2

--- app/src/main/java/com/example/laba3/MainActivity.kt
+++ app/src/main/java/com/example/laba3/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.laba3
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun Solve(view: View){
+
     }
 }
